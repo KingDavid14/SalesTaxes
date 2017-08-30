6f772787bbd72bc0c0cbdcff55ccdc326f6b863f
Valerio Emanuele <valerioemanuele@gmail.com>
2017-08-30 15:14:32 +0200

    Changed locale from US to default

--- src/com/ve/salestaxes/resources/BundleManager.java
+++ src/com/ve/salestaxes/resources/BundleManager.java
@@ -16,14 +16,14 @@ import org.apache.log4j.Logger;
 public class BundleManager {
 	
 	private static final BundleManager INSTANCE = new BundleManager();
-	private static final String BUNDLE_NAME = "com/ve/salestaxes/resources/MessagesBundle";
+	private static final String BUNDLE_NAME = "com.ve.salestaxes.resources.MessagesBundle";
 	
 	private static ResourceBundle messages;
 	private static final transient Logger log = Logger.getLogger(BundleManager.class);
 	
 	
 	private BundleManager(){
-		setBundleLocale(Locale.US);
+		setBundleLocale(Locale.getDefault());
 	}
 
 
